cf8d26d08c46f6cb1f7e3860c2a305558bc758e1
ilya-melnik <ilay.melnik.kr@gmail.com>
2021-06-01 15:30:29 +0300

    Accounting for viewed films START

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-const answer = [];
-answer[0] = prompt('How are you ?', "");
-answer[1] = prompt('How ?', "");
-answer[1] = prompt('Were ?', "");
+"use strict";
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели ?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: {},
+  privat: false,
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+  b = +prompt("На сколько оцените его ?", ""),
+  d = prompt("Один из последних просмотренных фильмов?", ""),
+  c = +prompt("На сколько оцените его ?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[d] = c;
+
+console.log(personalMovieDB);
