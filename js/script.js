bb5ab70ba4377344c980e7ec3a53c4a4c230efd5
ilya-melnik <ilay.melnik.kr@gmail.com>
2021-06-18 23:58:00 +0300

    add destructuring 2

--- js/script.js
+++ js/script.js
@@ -20,5 +20,26 @@ console.log(Object.keys(obj).length);
 obj.makeTest();
 
 //Destructuring
-const {border, background} = obj.colors;
+const { border, background } = obj.colors;
 console.log(border);
+
+// Destructuring example 2
+let options = {
+  size: {
+    width: 100,
+    height: 200,
+  },
+  items: ["Пончик", "Пирожное"],
+};
+
+let {
+  title = "test",
+  size: { width: w, height: h },
+  items: [item1, item2],
+} = options;
+
+console.log(title);
+console.log(w);
+console.log(h);
+console.log(item1);
+console.log(item2);
