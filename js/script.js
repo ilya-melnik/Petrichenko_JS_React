93d491ab3b4070e4053bbc8795e397724f29e07b
ilya-melnik <ilay.melnik.kr@gmail.com>
2021-07-17 13:53:04 +0300

    clone c помощью функции

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 "use strict";
-
+function copy(getObj){
+let newObj = {};
+let key;
+for(key in getObj){
+  newObj[key] = getObj[key];
+}
+return newObj;
+}
 
 let numbers = {
   a: 1,
@@ -7,7 +14,51 @@ let numbers = {
   c: 3,
   d: 4,
 };
+let newNum = copy(numbers);
+newNum.a = 10;
+console.log(newNum);
+console.log(numbers);
 
-const clone = Object.assign({}, numbers);
-clone.a = 43;
-console.log(clone);
+// let obj = {
+//   name: "test",
+//   age: 27,
+//   city: "Kiyv",
+//   colors: {
+//     border: "red",
+//     background: "grey",
+//   },
+//   //method of object
+//   makeTest: function () {
+//     console.log("test");
+//   },
+// };
+
+// // подсчет количества ключей в обьекте
+
+// console.log(Object.keys(obj).length);
+// obj.makeTest();
+
+// //Destructuring
+// const { border, background } = obj.colors;
+// console.log(border);
+
+// // Destructuring example 2
+// let options = {
+//   size: {
+//     width: 100,
+//     height: 200,
+//   },
+//   items: ["Пончик", "Пирожное"],
+// };
+
+// let {
+//   title = "test",
+//   size: { width: w, height: h },
+//   items: [item1, item2],
+// } = options;
+
+// console.log(title);
+// console.log(w);
+// console.log(h);
+// console.log(item1);
+// console.log(item2);
