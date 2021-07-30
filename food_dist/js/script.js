626fe0887381320c2fca7efff25e0ca5010073f0
ilya-melnik <ilay.melnik.kr@gmail.com>
2021-07-30 16:09:59 +0300

    41 Таймер обратного счета 2.0 (если время вышло, счетчик 00)

--- food_dist/js/script.js
+++ food_dist/js/script.js
@@ -56,6 +56,8 @@ window.addEventListener("DOMContentLoaded", () => {
   function getZero(num) {
     if (num >= 0 && num < 10) {
       return `0${num}`;
+    } else if (num <= 0) {
+      return `00`;
     } else {
       return num;
     }
