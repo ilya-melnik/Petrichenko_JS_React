3a271c6901ff39c74c6f2f7ce4ea6e5eeb4d24a0
ilya-melnik <ilay.melnik.kr@gmail.com>
2021-08-01 19:45:41 +0300

    43 Модальное окно (открывается и закрывается, плюс фиксируется страница overfolov: hidden)

--- food_dist/js/script.js
+++ food_dist/js/script.js
@@ -85,4 +85,23 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 
   setClock(".timer", dedline);
+
+  // Modal
+  const modalTrigger = document.querySelectorAll("[data-modal]"),
+    modal = document.querySelector(".modal"),
+    modalCloseBtn = document.querySelector("[data-close]");
+
+  modalTrigger.forEach((m) => {
+    m.addEventListener("click", () => {
+      modal.classList.add("show");
+      modal.classList.remove("hide");
+      document.body.style.overflow = "hidden";
+    });
+  });
+
+  modalCloseBtn.addEventListener("click", () => {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    document.body.style.overflow = "";
+  });
 });
