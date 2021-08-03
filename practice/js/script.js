4e241395c670113ad3b8cab58a0116fc76dcf910
ilya-melnik <ilay.melnik.kr@gmail.com>
2021-08-03 18:49:50 +0300

    47 class es6

--- practice/js/script.js
+++ practice/js/script.js
@@ -1,65 +1,31 @@
-"use strict";
-function showThis(a, b) {
-  console.log(this);
-  function sum() {
-    console.log(this);
-    //this.a + this.b = не правильно
-    return a + b;
+class Rectangle {
+  // концепция
+  constructor(height, width) {
+    this.height = height;
+    this.width = width; // если мы будем обращаться к this, мы будем обращаться к новому созданому обьекту
+  }
+  calckArea() {
+    return this.height * this.width;
   }
 }
-showThis(2, 3);
-// undefine undefine
-// 1) Обычная функция: this = window, но если 'use strict' - undefined
-const obj = {
-  a: 1,
-  b: 15,
-  sum: function () {
-    console.log(this);
-  },
-};
-obj.sum(); // return obj
-// 2) Контекст у методов обьекта - сам обьект
-// функция в нутри метода, имеет такой же контекст как и обычная функция
-// 3) this in constructor and class - it is new copy(экземпляр) obj
-// 4) ручная привяка this: call, apply, bind (bind создает новую функцию и под нее подвязывает контекст)
 
-// присвоение контекста в ручную
-function sayName(surname) {
-  console.log(this);
-  console.log(this.name + surname);
+class ColoredRectangleWithText extends Rectangle {
+  constructor(height, width, text, bgColor) {
+    super(height, width); // кпирует свойства у родителя, и нужные нужно передать
+    this.text = text;
+    this.bgColor = bgColor;
+  }
+
+  showMyProps() {
+    console.log(`Text: ${this.text}, цвет: ${this.bgColor}`);
+  }
 }
-const user = {
-  name: "Ilay",
-};
 
-sayName.call(user, "Melnik");
-sayName.apply(user, ["Melnik"]);
+const div = new ColoredRectangleWithText(25, 10, "Hello", "red");
+div.showMyProps();
+console.log(div.calckArea());
 
-function count (num) {
-  return this*num
-}
-const double = count.bind(2)
-// bind - this, double - num
-console.log(double(10));
-console.log(double(100));
-
-//----example----------
-const btn = document.querySelector("button");
-btn.addEventListener("click", function () {
-  console.log(this); // this - btn по которой кликнули(<button></button>). Works like even.target
-  this.style.backgroundColor = "red";
-});
-// стрелочная не имеет своего контекста, и берт его у родителя
-// если в обработчике событий используется => func. контекст вызова теряется
-const obj2 = {
-  num: 5,
-  sayNumber: function () {
-    const say = () => {
-      console.log(this.num);
-    };
-    say();
-  },
-};
-obj2.sayNumber();
-const double2 = a => a * 2;
-
+const square = new Rectangle(10, 10); //экземпляр
+const long = new Rectangle(20, 100);
+console.log(square.calckArea());
+console.log(long.calckArea());
