2dee1468a3d47cfded4e3236a08895372032ac29
ilya-melnik <ilay.melnik.kr@gmail.com>
2021-08-04 15:22:46 +0300

    49 spred, параметры по умолчанию

--- practice/js/script.js
+++ practice/js/script.js
@@ -1,31 +1,12 @@
-class Rectangle {
-  // концепция
-  constructor(height, width) {
-    this.height = height;
-    this.width = width; // если мы будем обращаться к this, мы будем обращаться к новому созданому обьекту
-  }
-  calckArea() {
-    return this.height * this.width;
-  }
+//rest
+const restOperator = function (a, b, ...rest) {
+  console.log(a, b, rest);
+};
+restOperator("a", "b", "c", "d", "e");
+
+// параметры по умолчанию, в случае если аргумент не был передан: basis = 4
+
+function calcOrDoble(number, basis = 4) {
+  console.log(number * basis);
 }
-
-class ColoredRectangleWithText extends Rectangle {
-  constructor(height, width, text, bgColor) {
-    super(height, width); // кпирует свойства у родителя, и нужные нужно передать
-    this.text = text;
-    this.bgColor = bgColor;
-  }
-
-  showMyProps() {
-    console.log(`Text: ${this.text}, цвет: ${this.bgColor}`);
-  }
-}
-
-const div = new ColoredRectangleWithText(25, 10, "Hello", "red");
-div.showMyProps();
-console.log(div.calckArea());
-
-const square = new Rectangle(10, 10); //экземпляр
-const long = new Rectangle(20, 100);
-console.log(square.calckArea());
-console.log(long.calckArea());
+calcOrDoble(2);
