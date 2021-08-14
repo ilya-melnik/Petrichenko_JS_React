b445c07fb89244b4c3410ce65a26a98b8a0c5348
ilya-melnik <ilay.melnik.kr@gmail.com>
2021-08-14 17:41:20 +0300

    53 отправка данных в JSON формате (изменен js и php)

--- food_dist/server.php
+++ food_dist/server.php
@@ -1,3 +1,6 @@
 <?php 
+    // для того чтобы на back-end получить данные в json:
+    $_POST = json_decode(file_get_contents("php://input"), true);
+    
     echo var_dump($_POST); // позволяет увидеть даные которые приходят с клиентом   
 ?>
