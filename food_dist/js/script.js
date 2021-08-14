b85e0ba420930e8829df21e9c51bd0500c480280
ilya-melnik <ilay.melnik.kr@gmail.com>
2021-08-14 17:00:40 +0300

    53 отправка данных в обычном формате

--- food_dist/js/script.js
+++ food_dist/js/script.js
@@ -212,4 +212,47 @@ window.addEventListener("DOMContentLoaded", () => {
     ".menu .container",
     "menu__item"
   ).render();
+
+  // Forms
+  const forms = document.querySelectorAll("form");
+  const message = {
+    loading: "Загрузка",
+    success: "Спасибо! Скоро мы с вами свяжемся!",
+    failure: "Что-то пошло не так",
+  };
+  // подвязка формы под запросы ниже
+  forms.forEach((item) => {
+    postData(item);
+  });
+  function postData(form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault(); //чтобы отменить перезагрузка страницы
+
+      let statusMessage = document.createElement("div");
+      statusMessage.classList.add("status");
+      statusMessage.textContent = message.loading;
+      form.appendChild(statusMessage);
+
+      const request = new XMLHttpRequest();
+      request.open("POST", "server.php");
+      //  new XMLHttpRequest() и setRequestHeader не устанавливается!!!, 
+      // request.setRequestHeader("Content-type", "multipart/from-data");
+      const formData = new FormData(form);
+
+      request.send(formData);
+
+      request.addEventListener("load", () => {
+        if (request.status === 200) {
+          console.log(request.response);
+          statusMessage.textContent = message.success;
+          form.reset(); 
+          setTimeout(()=>{
+            statusMessage.remove()
+          },2000)    
+        } else {
+          statusMessage.textContent = message.failure;
+        }
+      });
+    });
+  }
 });
